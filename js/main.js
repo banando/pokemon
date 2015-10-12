332b80e388d93e99e272a00318d330c588cc69ea
Fernando Orozco <forozco2085@gmail.com>
2015-10-12 09:28:41 -0700

    mirror html for player 1

--- js/main.js
+++ js/main.js
@@ -21,9 +21,14 @@ $('#p2Name').html(pokemon[3].name);
 
 
 $('#p1Pokemon').addClass('flipped');
+//input the stats value in table for comp/player2
 $('#hp2').html(pokemon[3].health*2);
 $('#ap2').html(pokemon[3].attack/2);
 $("#element2").html(pokemon[3].element);
+// input stats for player 1
+$('#hp1').html(pokemon[0].health*2);
+$('#ap1').html(pokemon[0].attack/2);
+$("#element1").html(pokemon[0].element);
 
 //special should not be used twice in a row
 //when used, make button disabled
